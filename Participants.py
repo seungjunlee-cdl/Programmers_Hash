9163cce148be0f88697cb4d9fa2b26b7a3efe0f0
Seungjun Lee <56711947+seungjunlee-cdl@users.noreply.github.com>
2022-02-11 11:37:24 +0900

    Add files via upload

---
+++ Participants.py
@@ -0,0 +1,13 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def solution(participant, completion):
+    answer = '' 
+
+    participant.sort() 
+    completion.sort() 
+
+    for i in range(len(completion)): 
+        if(participant[i] != completion[i]): 
+            return participant[i] 
+
+    return participant[len(participant)-1]
